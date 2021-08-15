630495b98d754b24066f924470d88321105734b5
Briidge <low1932219008@qq.com>
2021-08-15 23:24:55 +0800

    1.格式化代码 2.修正size为负数的情况

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,43 +1,47 @@
 public class ArrayDeque<T> {
-    Array items;
-    int capacity;
-    int size;
-    int nextFirst;
-    int nextLast;
-    public class Array{
-        T[] item;
-        public Array(int capacity){
+    private Array items;
+    private int capacity;
+    private int size;
+    private int nextFirst;
+    private int nextLast;
+
+    public class Array {
+        private T[] item;
+
+        public Array(int capacity) {
             item = (T[]) new Object[capacity];
         }
     }
-    public ArrayDeque (){
+
+    public ArrayDeque() {
         items = new Array(8);
         capacity = 8;
         nextFirst = 3;
         nextLast = 4;
         size = 0;
     }
-    public ArrayDeque (ArrayDeque other){
+
+    public ArrayDeque(ArrayDeque other) {
         items.item = (T[]) new Object[other.capacity];
         size = other.size;
         capacity = other.capacity;
         nextFirst = other.nextFirst;
         nextLast = other.nextLast;
-        for (int i = 0; i < other.capacity; i++){
+        for (int i = 0; i < other.capacity; i++) {
             items.item[i] = (T) other.items.item[i];
         }
     }
-    public void resizing (String str){
+
+    private void resizing(String str) {
         Array temp;
-        if (str.equals("increase")){
-            temp = new Array(capacity * 4);
-            for (int i = 0; i < capacity; i++){
+        if (str.equals("increase")) {
+            temp = new Array(capacity * 2);
+            for (int i = 0; i < capacity; i++) {
                 temp.item[i] = this.get(i);
             }
-        }
-        else {
+        } else {
             temp = new Array(capacity / 2);
-            for (int i = 0; i < size; i++){
+            for (int i = 0; i < size; i++) {
                 temp.item[i] = this.get(i);
             }
         }
@@ -46,46 +50,53 @@ public class ArrayDeque<T> {
         capacity = items.item.length;
         nextFirst = capacity - 1;
     }
-    public void addFirst(T item){
-        if (nextFirst == nextLast){
+
+    public void addFirst(T item) {
+        if (nextFirst == nextLast) {
             resizing("increase");
         }
         size += 1;
         items.item[nextFirst] = item;
-        if (nextFirst > 0){
+        if (nextFirst > 0) {
             nextFirst -= 1;
-        }
-        else {
+        } else {
             nextFirst = capacity - 1;
         }
     }
-    public void addLast(T item){
-        if (nextLast == nextFirst){
+
+    public void addLast(T item) {
+        if (nextLast == nextFirst) {
             resizing("increase");
         }
         size += 1;
         items.item[nextLast] = item;
-        if (nextLast < capacity - 1){
+        if (nextLast < capacity - 1) {
             nextLast += 1;
-        }
-        else {
+        } else {
             nextLast = 0;
         }
     }
-    public boolean isEmpty(){
+
+    public boolean isEmpty() {
         return size == 0;
     }
-    public int size(){
+
+    public int size() {
         return size;
     }
-    public void printDeque(){
-        for (int i = (nextFirst + 1) % capacity; i != nextLast; i = (i + 1) % capacity){
+
+    public void printDeque() {
+        for (int i = (nextFirst + 1) % capacity; i != nextLast; i = (i + 1) % capacity) {
             System.out.print(items.item[i] + " ");
         }
         System.out.println();
     }
-    public T removeFirst(){
-        if (capacity >= 16 && 1.0 * size / capacity < 0.25){
+
+    public T removeFirst() {
+        if (size == 0) {
+            return null;
+        }
+        if (capacity >= 16 && 1.0 * size / capacity < 0.25) {
             resizing("decrease");
         }
         size -= 1;
@@ -93,25 +104,29 @@ public class ArrayDeque<T> {
         nextFirst = (nextFirst + 1) % capacity;
         return res;
     }
-    public T removeLast(){
-        if (capacity >= 16 && 1.0 * size / capacity < 0.25){
+
+    public T removeLast() {
+        if (size == 0) {
+            return null;
+        }
+        if (capacity >= 16 && 1.0 * size / capacity < 0.25) {
             resizing("decrease");
         }
         size -= 1;
-        if (nextLast > 0){
+        if (nextLast > 0) {
             nextLast -= 1;
-        }
-        else {
+        } else {
             nextLast = capacity - 1;
         }
         return items.item[nextLast];
     }
-    public T get(int index){
+
+    public T get(int index) {
         int i = (nextFirst + 1) % capacity;
-        if (index + 1 > size){
+        if (index + 1 > size) {
             return null;
         }
-        for (; i != nextLast && index > 0; i = (i + 1) % capacity){
+        for (; i != nextLast && index > 0; i = (i + 1) % capacity) {
             index--;
         }
         return items.item[i];
